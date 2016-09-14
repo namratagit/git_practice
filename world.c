9cac3d35c555a7a1e0cf5e59d11eba6cc9c34f27
namratasys <namratasys@gmail.com>
2016-09-14 15:34:00 +0530

    chnages made in world.c

--- world.c
+++ world.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+int add (int a, int b)
+{
+	int sum = a+b;
+	printf ("%d\n", sum);
+}
+
+
 
 int main()
 {
 	printf ("hello world");
+	add (5,5);
 	return 0;
 }
