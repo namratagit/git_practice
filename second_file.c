389f6aa9b13649c8d5cca4d64554c1ca5363b903
namratasys <namratasys@gmail.com>
2016-09-14 15:32:12 +0530

    chnaged the second_file.c

--- second_file.c
+++ second_file.c
@@ -1,1 +1,19 @@
-this file is for second branch for demostraion purpose.
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int add (int a , int b)
+{
+	int sum = a + b; 
+	return sum;
+
+}
+int main ()
+{
+	int val = 0;
+	printf("hello world\n");
+	val = add(5 , 7);
+	printf ("%d\n" , val);
+	return 0; 
+}
+
